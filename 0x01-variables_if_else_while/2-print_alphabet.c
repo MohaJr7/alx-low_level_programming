e5c97112e41f55d6ef13870ee4a6447c5da4a170
MohaJr7 <abmmoha007@gmail.com>
2022-03-12 07:36:20 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main body
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
