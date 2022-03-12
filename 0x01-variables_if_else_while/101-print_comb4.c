a4009a5aa92d64fb9b7804ea254f1e679ee94f03
MohaJr7 <abmmoha007@gmail.com>
2022-03-12 12:37:41 -0800

    101-print

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - main header
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+	int p;
+
+	for (m = 48; m < 58; m++)
+	{
+		
+		for (n = m + 1; n < 58; n++)
+		{
+			for (p = n + 1; p < 58; p++)
+			{
+				if (m != 48 || n != 49 || p != 50)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				putchar(m);
+				putchar(n);
+				putchar(p);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
