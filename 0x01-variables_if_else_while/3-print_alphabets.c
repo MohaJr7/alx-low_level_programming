8f4b8aab2f3921e3ddee1dc704654a8e9663f4e5
MohaJr7 <abmmoha007@gmail.com>
2022-03-12 07:57:19 -0800

    3-prints

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - main part
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n =97; n < 123; n++)
+	{
+		putchar(n);
+	}
+	for (m = 65; m < 91; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
