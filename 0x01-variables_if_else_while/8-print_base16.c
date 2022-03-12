467b0e767af796f0a98b24daae253e02f1d22bc2
MohaJr7 <abmmoha007@gmail.com>
2022-03-12 12:06:08 -0800

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - main body
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m < 103; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return(0)
+}
