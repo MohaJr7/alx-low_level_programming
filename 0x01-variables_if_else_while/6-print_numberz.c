33d01ff5283ab686040ce89b15c86a2ad2eb43cc
MohaJr7 <abmmoha007@gmail.com>
2022-03-12 08:20:30 -0800

    6numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - main body
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n =48; < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
