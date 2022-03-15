2a18415f9ba0ca61322cc5497a2288e1e30b3e35
MohaJr7 <abmmoha007@gmail.com>
2022-03-15 16:50:36 -0700

    2-print_alohabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - main block
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 10;
+
+	while (i < 10)
+	{
+		char n = 'a';
+
+		while (n <= 'z')
+		{
+			_putchar(n);
+			n++;
+		}
+		_putchar('\n');
+		i++
+	}
+}
