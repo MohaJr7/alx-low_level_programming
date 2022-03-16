42d258e1f44c342d4f7f6dfc2dc89c3383c97d04
MohaJr7 <abmmoha007@gmail.com>
2022-03-15 17:10:07 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * @n : function variable
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else 
+		return (n);
+}
