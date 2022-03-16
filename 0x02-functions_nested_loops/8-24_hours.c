64f06920957a1ddc51e93be635f9220d1341fb2b
MohaJr7 <abmmoha007@gmail.com>
2022-03-15 17:19:44 -0700

    8hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * jack_bauer - main function
+ *
+ * Return: 0
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			int k;
+			int l;
+			int m;
+			int n;
+
+			k = i / 10;
+			l = i % 10;
+			m = j / 10;
+			n = j % 10;
+
+			_putchar('0' + k);
+			_putcahr('0' + l);
+			_putchar(':');
+			_putchar('0' + m);
+			_putchar('0' + n);
+			_putchar('\n');
+
+			j++'
+		}
+		i++;
+	}
+}
+
