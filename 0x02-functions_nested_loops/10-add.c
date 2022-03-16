ad22e1402e121d8cc5a8d5598eafa22ea821eaae
MohaJr7 <abmmoha007@gmail.com>
2022-03-15 17:30:09 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - main function
+ * @a : function variable
+ * @b : function variable
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+ *
